ee6b73c37520ef5cc5c1be335ed1f1558248a792
Nigina Zaripova <zaripovanigina1@gmail.com>
2024-09-22 18:40:15 +0300

    Удаляет коммиты

--- functions.js
+++ functions.js
@@ -1,5 +1,4 @@
 const checkString = function (myString, myLength) {
-  // проверяю символы и длину
   if (myString.length <= myLength) {
     return true;
   } else {
@@ -8,15 +7,10 @@ const checkString = function (myString, myLength) {
 };
 checkString('Добрыйдень', 20);
 
-// «Нормализовать» полученную строку, для чего:
-// let stringNew = ' ';
+
 const checkPallindrome = function (stringNew) {
-  // убрать с помощью метода replaceAll() все пробелы,
-  // привести строку к верхнему (метод toUpperCase()) или к нижнему (метод toLowerCase()) регистру,
   stringNew = stringNew.replaceAll(' ', '').toUpperCase();
-  // записать полученную строку в новую переменную.
   let stringEmpty = ' ';
-  // делаю обратный цикл, переворачиваю слово
   for (let i = stringNew - 1; i >= 0; i--) {
     stringEmpty += stringNew.at(i);
   }
@@ -24,4 +18,4 @@ const checkPallindrome = function (stringNew) {
 };
 
 checkPallindrome('Нигин');
-console.log(checkPallindrome('Нигин'));
+
