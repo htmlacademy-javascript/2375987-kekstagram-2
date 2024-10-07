d411745743b2d3710c971ed3c49079c1b32d5baa
Nigina Zaripova <zaripovanigina1@gmail.com>
2024-10-08 02:23:41 +0300

    полностью исправляет весь код

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
+// id Это число от 1 до 25. Идентификаторы не должны повторяться.
+// const idProfile = [];
+// const idTotal = 25;
+// for (let i = 1; i <=25; i++){
+//  idProfile.push(i);
+// };
 
-
-// id случайные от 1 до 15
-const idProfile = [];
-const idTotal = 25;
-for(let i = 1; i <= idTotal; i++) {
-  idProfile.push(i);
-}
-
-// вставила в массив картинки
-const avatarProfile = [
-  'img/avatar-1.svg',
-  'img/avatar-2.svg',
-  'img/avatar-3.svg',
-  'img/avatar-4.svg',
-  'img/avatar-5.svg',
-  'img/avatar-6.svg',
-];
-
-// описание к аватару
+// description, строка — описание фотографии
 const descriptionProfile = [
   'Отдыхаю',
   'Каникулы!',
@@ -30,14 +18,6 @@ const descriptionProfile = [
   'Классно',
 ];
 
-// количество лайков,
-const likesProfile = [];
-const likesTotal = 200;
-for (let j = 15; j <= likesTotal; j++){
-  likesProfile.push(j);
-}
-
-// комментарии к фото
 const messageProfile = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -55,34 +35,39 @@ const nameProfile = [
   'Марина',
   'Сережа',
 ];
-// Рандомайзер случайного числа,
+
+// рандомайзер числа из массива
 const randomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
+// ренерирую число, дальше буду подставлять, не уверена
+function getRandomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// создаю функцию, где мы будем создавать объект
 const createProfile = () => {
-  const randomId = randomInteger (0, idProfile.length - 1);
-  const randomAvatar = randomInteger (0, avatarProfile.length - 1);
-  const randomDescription = randomInteger (0, descriptionProfile.length - 1);
-  const randomLikes = randomInteger (0, likesProfile.length - 1);
-  const randomMessage = randomInteger (0, messageProfile.length - 1);
-  const randomName = randomInteger (0, nameProfile.length - 1);
-
-  // возвращаем объект, в котором  обращения к индексам массива
+  const randomDescription = randomInteger(0, descriptionProfile.length - 1);
+  const randomMessage = randomInteger(0, messageProfile.length - 1);
+  const randomName = randomInteger(0, nameProfile.length - 1);
   return {
-    id: idProfile[randomId],
-    avatar: avatarProfile[randomAvatar],
+    id: getRandomInRange(1, 25),
+    url: `photos/${getRandomInRange(1,25)}.jpg`,
     description: descriptionProfile[randomDescription],
-    likes: likesProfile[randomLikes],
-    message:messageProfile[randomMessage],
-    name: nameProfile[randomName],
+    likes: getRandomInRange(15, 200),
+
+    comments: [{
+      idComments: getRandomInRange(10, 80),
+      avatar: `img/avatar-${getRandomInRange(1,6)}.svg.`,
+      message: messageProfile[randomMessage],
+      name: nameProfile[randomName],
+    }]
   };
 };
-const quantityCount = 25;
-const newProfile = Array.from({length:quantityCount}, createProfile);
+const countNumber = 25;
+const newProfile = Array.from({
+  length: countNumber
+}, createProfile);
 console.log(newProfile);
-
